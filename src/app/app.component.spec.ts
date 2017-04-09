5ed01fcbdcac89f1e8d2c6a0bb36a44b59475549
Dang Truong Thi <thi.dtr@gmail.com>
2017-04-09 15:03:51 +0700

    Upgrade angular + angular cli version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,14 +2,13 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    });
-    TestBed.compileComponents();
-  });
+    }).compileComponents();
+  }));
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
