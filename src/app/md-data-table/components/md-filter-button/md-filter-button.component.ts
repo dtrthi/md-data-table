5e30648da3835cc9a1eb994fb1e6977700b928ca
Dang Truong Thi <thi.dtr@gmail.com>
2017-05-11 14:47:34 +0700

    Make filter button separated component

---
+++ src/app/md-data-table/components/md-filter-button/md-filter-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'md-filter-button',
+  templateUrl: './md-filter-button.component.html',
+  styleUrls: ['./md-filter-button.component.scss']
+})
+export class MdFilterButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/md-data-table/md-data-table.module.ts
+++ src/app/md-data-table/md-data-table.module.ts
@@ -12,6 +12,7 @@ import { InlineDialogComponent } from './components/inline-dialog/inline-dialog.
 import { MdDataTableComponent } from './components/md-data-table/md-data-table.component';
 import { MdDataColumnComponent } from './components/md-data-column/md-data-column.component';
 import { MdDataTableRowComponent } from './components/md-data-table-row/md-data-table-row.component';
+import { MdFilterButtonComponent } from './components/md-filter-button/md-filter-button.component';
 import { MdPaginatorComponent } from './components/md-paginator/md-paginator.component';
 import { MdTableCellDirective } from './directives/md-table-cell.directive';
 import { MdTableHeaderComponent } from './components/md-table-header/md-table-header.component';
@@ -34,6 +35,7 @@ import { MdTableHeaderComponent } from './components/md-table-header/md-table-he
     MdDataTableComponent,
     MdDataColumnComponent,
     MdDataTableRowComponent,
+    MdFilterButtonComponent,
     MdPaginatorComponent,
     MdTableCellDirective,
     MdTableHeaderComponent
